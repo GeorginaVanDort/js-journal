ab9b6564728f9aafc75b82996b16ba1b7ffb0446
Georgina Van Dort and Shradha Pulla <georgina+shradha@>
2016-08-01 10:59:59 -0700

    Create constructor & prelim wordCOunt method, add front-end js script

---
+++ js/journal-interface.js
@@ -0,0 +1,12 @@
+var Journal = require("./../js/journal.js").journalModule;
+
+$(document).ready(function(){
+  $("#word-count-form").submit(function(event) {
+    event.preventDefault();
+    var inputTitle = $("#title").val();
+    var inputBody = $("#body").val();
+    var journalWordCount = new Journal ("Containment", "Deadly viral outbreak in Atlanta Georgia");
+    var finalWordCount = journalWordCount.wordCount(inputBody);
+    $("#word-count-result").text(finalWordCount);
+  });
+});
---
+++ js/journal.js
@@ -0,0 +1,15 @@
+function Journal(newTitle, newBody) {
+  this.newTitle = newTitle;
+  this.newBody = newBody;
+}
+
+Journal.prototype.wordCount = function (inputBody) {
+  var wordCount = 0;
+  var bodyArray = inputBody.split(" ");
+  for (var i = 0; i < bodyArray.length; i ++) {
+    wordCount += 1;
+  }
+  return wordCount;
+}
+
+exports.journalModule. = Journal;
